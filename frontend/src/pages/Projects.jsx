81d828417ade28f35821a6098d7dcf3edb480f87
A <757818204@qq.com>
2024-08-06 00:11:37 +0800

    实现了一点点项目界面

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Project from '../widgets/Project.jsx'
+import Project from '../widgets/Project'
 function Projects() {
   return (
     <div className="bg-[url('./src/assets/image/projectsBackground.png')] bg-cover min-h-screen text-green-900 text-base flex">
-        <Project task="123"/>
+        <Project project={123}/>
         <div className="w-1/12 blurGlass rounded-lg flex flex-col">
         123
         </div>
--- frontend/src/widgets/Project.jsx
+++ frontend/src/widgets/Project.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import '../index.css'
-function project(task) {
+function Project({project}) {
   return (
     <div className="blurGlass rounded-lg w-1/4 mt-3 pb-5 px-2 flex flex-col">
         <input type="text" className='mb-3 w-full my-2
         bg-white rounded border border-gray-300 focus:border-green-400 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-green-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'>
-            {task}
+            
         </input>
         <div>
             任务1
@@ -14,4 +14,4 @@ function project(task) {
   )
 }
 
-export default project
+export default Project
